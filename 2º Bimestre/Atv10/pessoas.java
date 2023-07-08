e16cb362591c867387cfa762911e08bdc764841f
Luiz Felipe <124799524+Leuis0101@users.noreply.github.com>
2023-07-08 00:07:27 -0300

    Update pessoas.java

--- 2º Bimestre/Atv10/pessoas.java
+++ 2º Bimestre/Atv10/pessoas.java
@@ -1,30 +1,36 @@
 package Java.Atv010;
 
-public class pessoas {
+public class pessoas { // classe publica "pessoas"
     private String name;
     private int idade;
     private double alt;
 
+    // pega o nome
     public String getNome(){
         return name;
     }
 
+    // pega a idade
     public int getIdade(){
         return idade;
     }
-
+    
+    // pega a altura
     public String getAlt(){
         return String.format("%.2f", alt);
     }
 
+    // define o nome
     public void setNom(String name){
         this.nome = nome;
     }
-
+    
+    // define a idade
     public void setIdad(int idade){
         this.idade = idade;
     }
-
+    
+    // define a altura
     public void setAlt(double alt){
         this.alt = alt;
     }
