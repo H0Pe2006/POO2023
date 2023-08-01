2d60d80dce4ea5c452dab42f75f5c169ec17d22e
Luiz Felipe <124799524+Leuis0101@users.noreply.github.com>
2023-07-31 22:10:11 -0300

    Create Cachorros.java

---
+++ 3° Bimestre/Atv01/Cachorros.java
@@ -0,0 +1,22 @@
+public class Cachorros extends Mamiferos{
+    private String raca; //string privada "raca"
+
+    public Cachorros(String breed, String nom, int age){
+        super(nom, age);
+        this.raca = breed;
+    }
+
+    //retorna a string "raca"
+    public String getRaca(){
+        return this.raca;
+    }
+
+    public void setRaca(String breed){
+        this.raca = breed;
+    }
+
+    //printa a ação de "abanar o rabo" do animal
+    public void abanarRabo(){
+        System.out.println("Abanando o rabo...");
+    }
+}
